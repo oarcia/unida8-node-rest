46d1951cd33c8f5ece7bf7caf7255acc1653ddcd
oarcia <oarcia99@gmail.com>
2020-04-13 02:15:20 -0500

    se agrega la url por default /

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //parse application/json
 app.use(bodyParser.json());
 
+app.get('/', function(req, res) {
+    res.send('hola_mundo')
+});
 
 app.get('/usuario', function(req, res) {
     res.json('get usuario')
