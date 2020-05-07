f50067f39484bd92473928e3931207efb37b3ebb
oarcia <oarcia99@gmail.com>
2020-05-07 02:44:56 -0500

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -40,11 +40,31 @@ let verificaRole = (req, res, next) => {
     }
 };
 
+//====================================
+//verifica token imagen
+//====================================
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
 
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no valido'
+                }
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+    })
+
+};
 
 
 
 module.exports = {
     verificacionToken,
-    verificaRole
+    verificaRole,
+    verificaTokenImg
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,10 @@ app.use(require('./login'));
 app.use(require('./categoria'));
 //agregamos la ruta de productos
 app.use(require('./producto'));
+//sube imagenes
+app.use(require('./upload'));
+//checa imagenes
+app.use(require('./imagenes'));
 
 
 module.exports = app
